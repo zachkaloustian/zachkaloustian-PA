65abdc54c3b5627d21bbae4a825dcee3eb3acea2
zachkaloustian <zackal9039@students.ecpi.edu>
2025-02-26 11:04:00 -0500

    last commit

--- 5.6-performance-assessment/src/App.js
+++ 5.6-performance-assessment/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import myImage from './my-image.jpg'; // Import the image from the src folder
+import myImage from './my-image.jpg'; // Importing the image from the src folder
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Replace React logo with your image */}
+        {/* Replacing React logo with my image */}
         <img src={myImage} alt="replaced logo" className="App-logo" />
         <p>
           My image displayed
